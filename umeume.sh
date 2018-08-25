6055253fcd2caef5021e2d67149035506ecf7e05
梅宮 <euthanasia045@gmail.com>
2018-08-25 22:45:05 +0900

    自動パス通しスクリプト追加

--- umeume.sh
+++ umeume.sh
@@ -1,8 +1,3 @@
 #!bin/sh
 
-user_name = $(whoami)
-mkdir /Users/$user_name/.umeume
-shell_profile_path = $(find ~/.bash_profile)
-echo "\nexport PATH=$PATH:/Users/$user_name/.umeume"
-
 echo 'umeume'
