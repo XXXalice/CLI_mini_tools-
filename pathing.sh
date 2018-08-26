138c34e86b6c231f0238255c7521f31ad19b567d
梅宮 <euthanasia045@gmail.com>
2018-08-26 23:06:04 +0900

    path自動通しのバグ修正

--- pathing.sh
+++ pathing.sh
@@ -3,6 +3,7 @@
 user_name=$(whoami)
 mkdir -p /Users/$user_name/.umeume
 shell_profile_path=$(find ~/.bash_profile)
-full_path=PATH=$PATH:/Users/$user_name/.umeume
-echo "\nexport $full_path" >> $shell_profile_path
+full_path='PATH=$PATH:/Users/$user_name/.umeume'
+echo "export $full_path" >> $shell_profile_path
+source ~/.bash_profile
 cp umeume.sh /Users/$user_name/.umeume
